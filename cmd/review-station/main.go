c58ca2f600e438543d1ca33c8e857eb7b35fdd45
Felipe Paraizo <felipeparaizo@gmail.com>
2026-05-24 15:32:02 -0300

    feat: wire up entrypoint with config, client, and model

--- cmd/review-station/main.go
+++ cmd/review-station/main.go
@@ -5,16 +5,29 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+
+	"paraizofelipe/review-station/internal/config"
+	"paraizofelipe/review-station/internal/gitlab"
+	"paraizofelipe/review-station/internal/model"
 )
 
-type emptyModel struct{}
-
-func (m emptyModel) Init() tea.Cmd                           { return nil }
-func (m emptyModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return m, nil }
-func (m emptyModel) View() string                            { return "" }
-
 func main() {
-	p := tea.NewProgram(emptyModel{}, tea.WithAltScreen())
+	cfgPath := config.DefaultPath()
+	cfg, err := config.Load(cfgPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao carregar config em %s: %v\n\n", cfgPath, err)
+		fmt.Fprintf(os.Stderr, "Crie o arquivo:\n\n")
+		fmt.Fprintf(os.Stderr, "[gitlab]\nbase_url = \"https://gitlab.com\"\ntoken = \"glpat-...\"\n\n[[repo]]\nname = \"meu-projeto\"\npath = \"org/meu-projeto\"\nlocal = \"~/projects/meu-projeto\"\n")
+		os.Exit(1)
+	}
+
+	client := gitlab.NewClient(cfg.GitLab.BaseURL, cfg.GitLab.Token)
+	m := model.New(cfg, client)
+
+	p := tea.NewProgram(m,
+		tea.WithAltScreen(),
+		tea.WithMouseCellMotion(),
+	)
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "erro: %v\n", err)
 		os.Exit(1)
